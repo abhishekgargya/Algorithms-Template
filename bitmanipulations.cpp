1a27960e459e2e9385ce6b56a5bab7a546a49028
Abhishek Gargya <abhishekg0708@gmail.com>
2024-12-31 17:03:18 +0530

    initial commit

---
+++ bitmanipulations.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Bit-Manipulations
+#define setbit(N, j) (N |= (1 << j))
+#define checkbit(N, j) (N & (1 << j))
+#define bitoff(N, j) (N &= ~(1 << j))
+#define bittoggle(N, j) (N ^= (1 << j))
+#define LSBvalue(N) (N & (-N))
+#define highbits(N) ((1 << j) - 1)
+#define twopower(N) (!(N & (N - 1)))
+
+int main()
+{
+    // code here
+    return 0;
+}
