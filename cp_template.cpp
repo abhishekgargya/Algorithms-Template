d996b9189f7dda53dbab6695b4a0fbfc8fd3a9cb
Abhishek Gargya <abhishekg0708@gmail.com>
2025-01-15 22:48:13 +0530

    added ordered set

--- cp_template.cpp
+++ cp_template.cpp
@@ -1,5 +1,13 @@
 #include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
 using namespace std;
+using namespace __gnu_pbds;
+
+template <class T>
+using oset = tree<T, null_type, less<T>,                           // less,greater,less_equal
+                  rb_tree_tag, tree_order_statistics_node_update>; // find_by_order,order_of_key
 
 int sum() { return 0; }
 
